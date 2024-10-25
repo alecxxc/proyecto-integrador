35162543ce4c5e9fa934b3f1d12d4696bb4abaf3
Edwin Garcia <garciaalexandro449@gmail.com>
2024-10-24 22:56:45 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/integrative_project/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.integrative_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void ir (View v){
+        Intent i = new Intent(this, login_activity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/integrative_project/login_activity.java
@@ -0,0 +1,25 @@
+package com.example.integrative_project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login_activity extends AppCompatActivity {
+private EditText pt_nombre, et_contraseña;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        pt_nombre = (EditText) findViewById(R.id.pt_name);
+        et_contraseña = (EditText) findViewById(R.id.et_password);
+    }
+
+    public void Ingresar (View view){
+        Intent i = new Intent(this, second_activity.class);
+        startActivity(i);
+    }
+}
