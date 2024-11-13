63344a8a19acdc63bf02cc6b634517560be58193
Juan <anonimo7510@gmail.com>
2024-11-13 00:09:06 -1200

    ..............

---
+++ app/src/main/java/com/example/integrative_project/evento2.java
@@ -0,0 +1,12 @@
+package com.example.integrative_project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class evento2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.evento2); // Aquí cargas el layout evento2.xml
+    }
+}
